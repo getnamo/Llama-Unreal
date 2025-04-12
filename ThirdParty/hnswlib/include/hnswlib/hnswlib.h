150e297add7bba357b1676ec4762ec031b6c5966
getnamo <getnamo@gmail.com>
2025-04-12 02:00:45 -0700

    disable C4668

--- ThirdParty/hnswlib/include/hnswlib/hnswlib.h
+++ ThirdParty/hnswlib/include/hnswlib/hnswlib.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#pragma warning(disable: 4668)
+
 // https://github.com/nmslib/hnswlib/pull/508
 // This allows others to provide their own error stream (e.g. RcppHNSW)
 #ifndef HNSWLIB_ERR_OVERRIDE
