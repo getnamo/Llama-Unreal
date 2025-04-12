4ffc22416a384e79f46e11c995eb8a5414e682d6
getnamo <getnamo@gmail.com>
2025-04-12 02:06:18 -0700

    use https://github.com/nmslib/hnswlib/blob/master/examples/cpp/EXAMPLES.md for basics test

---
+++ Source/LlamaCore/Public/Embedding/VectorSearch.h
@@ -0,0 +1,17 @@
+// Copyright 2025-current Getnamo.
+
+#pragma once
+
+/** 
+* Unreal style native wrapper for HNSW nearest neighbor search for high dimensional vectors
+*/
+class FVectorSearch
+{
+public:
+
+    //Simple test to see if the basics run
+    void BasicsTest();
+
+    FVectorSearch();
+    ~FVectorSearch();
+};
